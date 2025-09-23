215ec11a24e44ac19fda119b3c7b5aebba00705c
Arpit Singhal <arpitsinghal4002@gmail.com>
2025-09-23 14:39:21 +0530

    new update

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
-import { adminLogin } from '../controllers/adminController.js';
+import { adminLogin, approveCommentById, getAllBlogsAdmin, getAllComments } from '../controllers/adminController.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/blogs", auth, getAllBlogsAdmin);
+adminRouter.post("/delete-comment", auth, deleteCommentById);
+adminRouter.post("/approve-comment", approveCommentById);
+adminRouter.get("/dashboard", auth, getDashboard);
+
+
 
 export default adminRouter;
